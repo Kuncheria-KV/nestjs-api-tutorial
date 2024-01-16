8cdd175104a88d0737051400e0690796fdcc963e
Kuncheria-KV <kuncheria@keyvalue.systems>
2024-01-16 14:38:03 +0530

    Add user edit endpoint

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,6 +4,7 @@ import { AppModule } from 'src/app.module';
 import * as pactum from 'pactum';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from 'src/auth/dto';
+import { EditUserDto } from 'src/user/dto';
 describe('App e2e', () => {
   let app: INestApplication;
   let prisma: PrismaService;
@@ -125,7 +126,24 @@ describe('App e2e', () => {
       });
     });
 
-    describe('Edit user', () => {});
+    describe('Edit user', () => {
+      it('should edit user', () => {
+        const dto: EditUserDto = {
+          firstName: 'Loki Odinson',
+          email: 'loki@asgard.com',
+        };
+        return pactum
+          .spec()
+          .patch('/users')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .withBody(dto)
+          .expectStatus(200)
+          .expectBodyContains(dto.firstName)
+          .expectBodyContains(dto.email);
+      });
+    });
   });
 
   describe('Bookmark', () => {
